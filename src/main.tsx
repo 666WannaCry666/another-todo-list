9a25dcb3ff2ac4b607b162078602c18aaab2ec18
666WannaCry666 <usov3124@gmail.com>
2025-11-23 05:45:42 +0700

    Aliases added

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react';
 import { attachReduxDevTools } from '@effector/redux-devtools-adapter';
 import './index.css';
 import ReactDOM from 'react-dom/client';
+import App from '@/App';
 
 function renderApp() {
     ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <StrictMode>
-            
+            <App />
         </StrictMode>
     );
 }
